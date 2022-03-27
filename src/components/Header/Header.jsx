9fee5df9c2968e4fa12895f2beec3fb6a0ff560f
GST-Slava <gstslava@gmail.com>
2022-03-27 23:46:45 +0300

    api axios get server

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import s from './Header.module.css';
 
 const Header = () => {
     return <header className={s.header}>
-        <img src='https://i.pinimg.com/originals/4f/d3/50/4fd3509513507f97cc7b30edc5cc910f.jpg' alt='logo_gst' />
+        <img src='https://www.look.com.ua/large/201209/14249.jpg' alt='logo_gst' />
     </header>
 }
 
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import styles from './users.module.css';
+import axios, { Axios } from "axios";
+import userPhoto from '../../assets/images/av_default.jpg';
 
 let Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-            { id: 1, photoUrl: 'https://www.meme-arsenal.com/memes/825b9b3ef148fbf2b20f47951e170699.jpg', followed: false, fullName: 'Dmitry', status: 'I am a ceo', location: { city: 'Minsk', country: 'Belarus' } },
-            { id: 2, followed: true, fullName: 'Alexander', status: 'I am freelancer', location: { city: 'St.Peterburg', country: 'Russia' } },
-            { id: 3, followed: false, fullName: 'Igor', status: 'I am a builder', location: { city: 'St.Peterburg', country: 'Russia' } },
-            { id: 4, followed: true, fullName: 'Viacheslv', status: 'I am a developer', location: { city: 'Minsk', country: 'Belarus' } },
-        ]
-        )
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items);
+        });
     }
 
     return <div>
@@ -18,7 +17,7 @@ let Users = (props) => {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto} />
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
                     </div>
                     <div>
                         {u.followed
@@ -30,12 +29,12 @@ let Users = (props) => {
 
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
 
